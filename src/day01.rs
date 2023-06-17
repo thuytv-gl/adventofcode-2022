e668f4a6518ac2261834d61d281e6eb0db7a3bd7
thuy tran van <thuytranvan.gl@gmail.com>
2023-06-18 02:05:08 +0700

    finish day 01

--- src/day01.rs
+++ src/day01.rs
@@ -2,30 +2,41 @@ use std::fs;
 
 pub fn solution() {
     let chars: String = fs::read_to_string("src/day01.input").unwrap().parse().unwrap();
-    let mut that_elf = (0, 0);
-    let mut counter: u32 = 0;
-    let mut position = 1;
     let input: Vec<&str> = chars.split("\n").collect();
-
+    let mut inventory: Vec<u32> = vec![];
+    let mut counter: u32 = 0;
     for s in input {
         let num = s.trim().parse::<u32>();
         match num {
             Ok(n) => counter += n,
             Err(e) => match e.kind() {
                 std::num::IntErrorKind::Empty => {
-                    if counter > that_elf.1 {
-                        that_elf.0 = position;
-                        that_elf.1 = counter;
-                    }
+                    inventory.push(counter);
                     counter = 0;
-                    position += 1;
                 },
                 _ => {},
             }
         }
     }
 
-    println!("The elf number {} carrying the most calories of {}",
-             that_elf.0, that_elf.1);
+    inventory.sort_by(|a, b| b.cmp(a));
+
+    println!(
+        "| {0: <10} | {1: <10} |",
+        "Name", "Value",
+    );
+    println!("| {0: <10} | {1: <10} |", "----------",  "----------");
+    if let Some(top1) = inventory.get(0) {
+        println!("| {0: <10} | {1: <10} |", "Top 1", top1);
+    }
+
+    let mut top3 = 0;
+    for i in 0..3 {
+        if let Some(calories) = inventory.get(i) {
+            top3 += calories;
+        }
+    }
+    println!("| {0: <10} | {1: <10} |", "Top 3", top3);
 }
+
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod day01;
+mod day02;
 
 fn main() {
-    day01::solution();
+    let day = 1;
+    match day {
+        1 => day01::solution(),
+        2 => day02::solution(),
+        _ => {},
+    }
 }
