df718623bfdcb59ccf59abbb3a2ef8e145eb33fb
thuy tran van <thuytranvan.gl@gmail.com>
2023-12-07 22:37:22 +0700

    day one part two

--- src/day01.go
+++ src/day01.go
@@ -7,7 +7,12 @@ import (
     "strconv"
 )
 
-func head(s string) string {
+// ============================================================
+//              Part One
+// ============================================================
+type PartOne struct {}
+
+func (self PartOne) head(s string) string {
     for _, c := range s {
         if unicode.IsNumber(rune(c)) {
             return string(c)
@@ -16,7 +21,7 @@ func head(s string) string {
     return ""
 }
 
-func tail(s string) string {
+func (self PartOne) tail(s string) string {
     for i := len(s) - 1; i >= 0; i-- {
         c :=s[i] 
         if unicode.IsNumber(rune(c)) {
@@ -26,7 +31,7 @@ func tail(s string) string {
     return ""
 }
 
-func main() {
+func (self PartOne) start() {
     dat, err := os.ReadFile("./src/day01.txt")
     if err != nil {
         panic(err)
@@ -34,11 +39,70 @@ func main() {
     puzzle := strings.Split(string(dat), "\r\n")
     result := 0
     for _, p := range puzzle {
-        n1 := head(p)
-        n2 := tail(p)
+        n1 := self.head(p)
+        n2 := self.tail(p)
         if n, err := strconv.Atoi(n1+n2); err == nil {
             result = result + n
         }
     }
-    fmt.Println(result)
+    fmt.Println("Part one:", result)
+}
+
+// ============================================================
+//              Part Two
+// ============================================================
+type PartTwo struct {}
+var numbers = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+func (self PartTwo) head(s string) string {
+    s_len := len(s)
+    for i, c := range s {
+        if unicode.IsNumber(rune(c)) {
+            return string(c)
+        }
+        for n, nt := range numbers {
+            if i + len(nt) < s_len &&  s[i:i+len(nt)] == nt {
+                return strconv.Itoa(n+1)
+            }
+        }
+    }
+    return ""
+}
+
+func (self PartTwo) tail(s string) string {
+    for i := len(s) - 1; i >= 0; i-- {
+        c :=s[i] 
+        if unicode.IsNumber(rune(c)) {
+            return string(c)
+        }
+
+        for n, nt := range numbers {
+            if (i - len(nt) > -2) && s[i-len(nt)+1:i+1] == nt {
+                return strconv.Itoa(n+1)
+            }
+        }
+    }
+    return ""
+}
+
+func (self PartTwo) start() {
+    dat, err := os.ReadFile("./src/day01_2.txt")
+    if err != nil {
+        panic(err)
+    }
+    puzzle := strings.Split(string(dat), "\r\n")
+    result := 0
+    for _, p := range puzzle {
+        n1 := self.head(p)
+        n2 := self.tail(p)
+        if n, err := strconv.Atoi(n1+n2); err == nil {
+            result = result + n
+        }
+    }
+    fmt.Println("Part two:", result)
+}
+
+func main() {
+    PartOne{}.start()
+    PartTwo{}.start()
 }
